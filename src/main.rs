05ae550864ddc658131011b41e32b0f2a8898eb2
Kevin Do <kevin.kydat.do@gmail.com>
2017-02-20 11:31:31 -0800

    Improve thread structure

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,43 @@ extern crate websocket;
 
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::sync::mpsc;
 use websocket::{Server, Message, Sender, Receiver};
 use websocket::message::Type;
 
 fn main() {
     let server = Server::bind("0.0.0.0:8000").unwrap();
 
-    let senders = Arc::new(Mutex::new(Vec::new()));
+    let txs = Arc::new(Mutex::new(Vec::new()));
 
     for connection in server {
         let request = connection.unwrap().read_request().unwrap();
         let response = request.accept();
-
-        let mut client = response.send().unwrap();
-        let ip = client.get_mut_sender()
+        let client = response.send().unwrap();
+        let (mut sender, mut receiver) = client.split();
+        let ip = sender
             .get_mut()
             .peer_addr()
             .unwrap();
         println!("Connection from {}", ip);
-        
-        let (sender, mut receiver) = client.split();
-        let sender = Arc::new(Mutex::new(sender));
 
-        let senders_clone = senders.clone();
-        let sender_clone = sender.clone();
+        let (tx, rx) = mpsc::channel();
+        let tx = Arc::new(Mutex::new(tx));
 
-        {
-            let mut senders = senders.lock().unwrap();
-            senders.push(sender);
-        }
+        let tx_to_push = tx.clone();
+        let txs_for_thread = txs.clone();
 
+        let mut txs = txs.lock().unwrap();
+        txs.push(tx_to_push);
+
+        // `sender` thread
+        thread::spawn(move || {
+            for message in rx {
+                sender.send_message(&message).unwrap();
+            }
+        });
+
+        // `receiver` thread
         thread::spawn(move || {
             for message in receiver.incoming_messages() {
                 let message: Message = message.unwrap();
@@ -39,21 +46,21 @@ fn main() {
                 match message.opcode {
                     Type::Close => {
                         let message = Message::close();
-                        let mut sender_clone = sender_clone.lock().unwrap();
-                        sender_clone.send_message(&message).unwrap();
+                        let tx = tx.lock().unwrap();
+                        tx.send(message).unwrap();
                         println!("Client {} disconnected", ip);
                         return;
                     },
                     Type::Ping => {
                         let message = Message::pong(message.payload);
-                        let mut sender_clone = sender_clone.lock().unwrap();
-                        sender_clone.send_message(&message).unwrap();
+                        let tx = tx.lock().unwrap();
+                        tx.send(message).unwrap();
                     }
                     _ => {
-                        let senders_clone = senders_clone.lock().unwrap();
-                        for sender in senders_clone.iter() {
-                            let mut sender = sender.lock().unwrap();
-                            sender.send_message(&message).unwrap()
+                        let txs = txs_for_thread.lock().unwrap();
+                        for tx in txs.iter() {
+                            let tx = tx.lock().unwrap();
+                            tx.send(message.clone()).unwrap();
                         }
                     }
                 }
