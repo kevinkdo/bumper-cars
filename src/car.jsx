633bf64a4f06c2b7f00ad47b4deb3e96bad4bfd8
Kevin Do <kevin.kydat.do@gmail.com>
2017-02-20 16:13:23 -0800

    webapps can talk to eachother, need to fix server memory leaks after client disconnect

---
+++ src/car.jsx
@@ -0,0 +1,121 @@
+var wsUri = "ws://127.0.0.1:8000/";
+
+const Playground = React.createClass({
+  websocket: new WebSocket(wsUri),
+
+  componentDidMount: function() {
+    var me = this;
+    me.setupKeyBindings();
+    
+    me.websocket.onopen = function(evt) { me.onWsOpen(evt) };
+    me.websocket.onclose = function(evt) { me.onWsClose(evt) };
+    me.websocket.onmessage = function(evt) { me.onWsMessage(evt) };
+    me.websocket.onerror = function(evt) { me.onWsError(evt) };
+  },
+
+  setupKeyBindings: function() {
+    var me = this;
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 37: // left
+          me.setState((state) => me.state.cars[me.state.id].x -= 5);
+        break;
+
+        case 38: // up
+          me.setState((state) => me.state.cars[me.state.id].y -= 5);
+        break;
+
+        case 39: // right
+          me.setState((state) => me.state.cars[me.state.id].x += 5)
+        break;
+
+        case 40: // down
+          me.setState((state) => me.state.cars[me.state.id].y += 5)
+        break;
+
+        default: return; // exit this handler for other keys
+      }
+
+      var update_message = {
+        id: me.state.id,
+        position: me.state.cars[me.state.id]
+      };
+      me.websocket.send(JSON.stringify(update_message));
+      e.preventDefault(); // prevent the default action (scroll / move caret)
+    });
+  },
+
+  onWsOpen: function() {
+    this.setState({
+      status: "Successfully connected to server."
+    });
+    this.websocket.send("getid");
+  },
+
+  onWsMessage: function(evt) {
+    if (evt.data.startsWith("getid")) {
+      const id = evt.data.slice(5);
+      var cars = this.state.cars;
+      cars[id] = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+      }
+      this.setState({
+        id: id,
+        cars: cars
+      });
+    } else {
+      var message = JSON.parse(evt.data);
+      if (message.id === this.state.id) {
+        return
+      }
+
+      var cars = this.state.cars;
+      cars[message.id] = message.position;
+      this.setState({
+        cars: cars
+      });
+    }
+  },
+
+  onWsClose: function(evt) {
+    this.setState({status: "No connection to server."});
+  },
+
+  onWsError: function(evt) {
+    alert("Error connecting to server: " + evt.data)
+  },
+
+  getInitialState: function() {
+    var me = this;
+
+    return {
+      id: null,
+      cars: {}, // id -> {x:, y:} objects
+      status: "Setting up connection to server..."
+    };
+  },
+
+  render: function() {
+    var me = this;
+    if (!this.state.id) {
+      return <div>{this.state.status}</div>;
+    }
+
+    const circles = Object.keys(this.state.cars).map(function(key) {
+      var position = me.state.cars[key];
+      return <circle r="5" fill="black" cx={position.x} cy={position.y} key={"car" + key} />
+    });
+
+    return (
+      <div>{this.state.status}
+        <svg className="playground" width={window.innerWidth} height={window.innerHeight}>
+          {circles}
+        </svg>
+      </div>
+    );
+  }
+});
+
+
+ReactDOM.render(<Playground />, document.getElementById('react_main'));
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 extern crate websocket;
 
+use std::str;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::sync::mpsc;
+use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 use websocket::{Server, Message, Sender, Receiver};
 use websocket::message::Type;
+use websocket::ws::dataframe::DataFrame;
+
+static GLOBAL_COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
 
 fn main() {
+    GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst);
     let server = Server::bind("0.0.0.0:8000").unwrap();
 
     let txs = Arc::new(Mutex::new(Vec::new()));
@@ -22,7 +28,7 @@ fn main() {
             .unwrap();
         println!("Connection from {}", ip);
 
-        let (tx, rx) = mpsc::channel();
+        let (tx, rx) = mpsc::channel::<Message>();
         let tx = Arc::new(Mutex::new(tx));
 
         let tx_to_push = tx.clone();
@@ -34,6 +40,9 @@ fn main() {
         // `sender` thread
         thread::spawn(move || {
             for message in rx {
+                let payload = message.payload();
+                let payload = str::from_utf8(payload.as_ref());
+                println!("> {:?}", payload);
                 sender.send_message(&message).unwrap();
             }
         });
@@ -57,11 +66,31 @@ fn main() {
                         tx.send(message).unwrap();
                     }
                     _ => {
-                        let txs = txs_for_thread.lock().unwrap();
-                        for tx in txs.iter() {
-                            let tx = tx.lock().unwrap();
-                            tx.send(message.clone()).unwrap();
-                        }
+                        let raw_payload = message.payload();
+                        let raw_payload = str::from_utf8(raw_payload.as_ref());
+                        println!("< {:?}", raw_payload);
+
+                        match raw_payload {
+                            // Reply with unique id only to the sender
+                            Ok("getid") => {
+                                let id_string = GLOBAL_COUNTER.fetch_add(1, Ordering::SeqCst).to_string();
+                                let prefix = "getid".to_string();
+                                let combined_string = prefix + &id_string;
+                                let message = Message::text(combined_string);
+                                let tx = tx.lock().unwrap();
+                                tx.send(message).unwrap()
+                            },
+                            // Broadcast the message if it's a valid UTF8 encoding
+                            Ok(_) => {
+                                let txs = txs_for_thread.lock().unwrap();
+                                for tx in txs.iter() {
+                                    let tx = tx.lock().unwrap();
+                                    tx.send(message.clone()).unwrap();
+                                }
+                            }
+                            // Ignore the message if it's not valid UTF8
+                            Err(_) => {}
+                        };
                     }
                 }
             }
